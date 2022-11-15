383c8c020fec105f34d362f4483162a5f275f9d7
moddingtres <gamingpro6942@gmail.com>
2022-11-15 14:10:36 +0100

    e

--- js/layers.js
+++ js/layers.js
@@ -24,5 +24,30 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+    upgrades: {
+        11: {
+            title: "Double Modding Point gain.",
+            description: "Double your point gain.",
+            cost: new Decimal(5),
+    upgrades: {
+             12: {
+                title: "Double Modding Point gain again",
+                description: "Double your point gain (again).",
+                cost: new Decimal(1),
+                upgrades: {
+                    13: {
+                        title: "Gives boost to Prestige Points gain based on Modding Points",
+                        description: "Double your point gain.",
+                        cost: new Decimal(1),
+                        effect() {
+                            return player[this.layer].points.add(1).pow(0.5)
+                        },
+                        effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect
+                    },
+                }, },
+            },
+        },
+    },
 })
+
--- js/mod.js
+++ js/mod.js
@@ -41,6 +41,9 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
+		if (hasUpgrade('p', 11)) gain = gain.times(2)
+		if (hasUpgrade('p', 12)) gain = gain.times(2)
+		if (hasUpgrade('p', 13)) gain = gain.times(upgradeEffect('p', 13))
 
 	let gain = new Decimal(1)
 	return gain

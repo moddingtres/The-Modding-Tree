dc44ae37246ada5a20a597d0f72b796a1105e408
moddingtres <gamingpro6942@gmail.com>
2022-11-15 14:00:19 +0100

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,8 +1,8 @@
 let modInfo = {
 	name: "The ??? Tree",
 	id: "mymod",
-	author: "nobody",
-	pointsName: "points",
+	author: "moddingtres",
+	pointsName: "Modding Point",
 	modFiles: ["layers.js", "tree.js"],
 
 	discordName: "",
@@ -13,8 +13,8 @@ let modInfo = {
 
 // Set your version in num and name
 let VERSION = {
-	num: "0.0",
-	name: "Literally nothing",
+	num: "0.1",
+	name: "legit nothing",
 }
 
 let changelog = `<h1>Changelog:</h1><br>
